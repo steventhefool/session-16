8175c0f695226e557aab0a84a035c988ec7991ae
steventhefool <hoangduongnampb.2k6@gmail.com>
2025-01-07 17:02:17 +0700

    Add files via upload

---
+++ bai 8.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+	char inputString[100], reverseString[100];
+	char *ptrrev, *ptrinput;
+	printf ("Nhap vao mot chuoi: ");
+	fgets (inputString,sizeof(inputString),stdin);
+	int n =strlen(inputString);
+	ptrrev=reverseString;
+	ptrinput=inputString;
+	for (int i=n-1;i>=0;i--){
+		if (*(ptrinput+i)!='/0'){    //Em de cho no neu no tro vao ky tu xuong dong thi no khong in nhung ma no van in ra, thay sua giup em voi a.
+		*(ptrrev++) = *(ptrinput+i);  
+	}
+	}
+	printf ("Chuoi dau tien la: \n%s", inputString);
+	printf ("Chuoi thu hai la: %s\n", reverseString);
+}
